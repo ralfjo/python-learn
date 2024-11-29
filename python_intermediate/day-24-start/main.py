f725c0791ebdc06e15eeb90c1ebe1e6b0650d871
ralfjo <ralf418@gmail.com>
2024-11-29 18:27:43 +0900

    day 24 lesson #1

---
+++ python_intermediate/day-24-start/main.py
@@ -0,0 +1,12 @@
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+with open("my_file.txt", mode="w") as file:
+    file.write("\nnew text.")
+    
