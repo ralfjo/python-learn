5388d4236fd3de7b8454035e4ae603e926ae81c2
ralfjo <ralfjo@ncsoft.com>
2024-12-06 09:23:58 +0900

    day 39 lesson #1

---
+++ python_intermediate/day-38-start/main.py
@@ -0,0 +1,36 @@
+import requests
+
+# https://docs.google.com/document/d/1_q-K-ObMTZvO0qUEAxROrN3bwMujwAN25sLHwJzliK0/edit#
+# https://docs.google.com/document/d/1_q-K-ObMTZvO0qUEAxROrN3bwMujwAN25sLHwJzliK0/edit#heading=h.gz6pu9o7f9iz
+# https://docs.google.com/document/d/1_q-K-ObMTZvO0qUEAxROrN3bwMujwAN25sLHwJzliK0/edit#heading=h.zhjgcprrgvim
+# https://gist.github.com/angelabauer/dd71d7072626afd728e1730584c6e4b8
+# https://gist.github.com/TheMuellenator/974c39779ec516c4c60e918c001e48ba
+
+GENDER = YOUR GENDER
+WEIGHT_KG = YOUR WEIGHT
+HEIGHT_CM = YOUR HEIGHT
+AGE = YOUR AGE
+
+APP_ID = YOUR APP ID
+API_KEY = YOUR API KEY
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
