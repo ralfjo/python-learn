875440e01039d2e5d32b611f80f7aeec8d6fdff8
ralfjo <ralf418@gmail.com>
2024-12-06 19:17:19 +0900

    day 39 lesson #2

---
+++ python_intermediate/day-39-start/main.py
@@ -0,0 +1,29 @@
+import requests
+from pprint import pprint
+
+# Sheety API URL
+SHEETY_API_URL = ""  # 여기에 Sheety API URL을 입력하세요.
+
+
+def fetch_google_sheet_data():
+    try:
+        # API 호출
+        response = requests.get(SHEETY_API_URL)
+        response.raise_for_status()  # 에러가 있으면 예외 발생
+
+        # 데이터 출력
+        data = response.json()
+        # pprint(data)
+        return data
+    
+    except requests.exceptions.RequestException as e:
+        print(f"API 호출 중 오류 발생: {e}")
+        return None
+
+# 함수 실행
+if __name__ == "__main__":
+    gsheet_data = fetch_google_sheet_data()
+    
+    sheet_data = gsheet_data["prices"]
+    pprint(sheet_data)
+
