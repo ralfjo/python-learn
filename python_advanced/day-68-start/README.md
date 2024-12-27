096db2f7414b34cccfd6ded401b9933cb17bbc16
ralfjo <ralfjo@ncsoft.com>
2024-12-27 14:02:47 +0900

    day 68 lesson #2

--- python_advanced/day-68-start/README.md
+++ python_advanced/day-68-start/README.md
@@ -49,3 +49,75 @@ https://cryptii.com/
 https://www.youtube.com/watch?v=G2_Q9FoD-oQ&ab_channel=Numberphile
 https://www.youtube.com/watch?v=V4V2bpZlqx8&ab_channel=Numberphile
 
+https://plaintextoffenders.com/
+https://haveibeenpwned.com/Passwords
+https://en.wikipedia.org/wiki/List_of_the_most_common_passwords
+http://password-checker.online-domain-tools.com/
+
+Salt Rounds - salt+hash 의 반복 회수
+비밀번호 솔팅  -- hash + salt
+
+# Werkzeug를 사용하여 비밀번호 해싱하기
+이 시점에서 사용자의 비밀번호는 데이터베이스에 일반 텍스트로 저장됩니다:
+1. 데이터베이스에서 해시되지 않은 이전 항목을 삭제하세요.
+비밀번호를 저장하기 전에 해시를 통해 사용자의 비밀번호를 보호합니다.
+이를 위해서는 벡자이크(Werkzeug)의 헬퍼 함수 generate_password_hash()를 사용합니다.
+2. 다음 링크의 문서를 사용하여 사용자 비밀번호를 해싱 및 솔트하는 방법을 알아내 보세요.
+https://werkzeug.palletsprojects.com/en/1.0.x/utils/#module-werkzeug.security
+pbkdf2:sha256을 사용하여 비밀번호를 해시하고
+솔트 길이(salt length) 8을 추가하세요.
+다음과 같은 결과나 나와야 합니다.
+
+해답 - https://gist.github.com/angelabauer/36b4437aec707e488131189c1ec18f7f
+
+
+
+# Flask_Login 패키지로 사용자 인증하기
+현재는 /secrets으로 이동하면 기밀 페이지와 다운로드 링크가 표시되며, 인증 장벽이 없습니다. 등록/로그인한 사용자만 해당 페이지를 보고 파일을 다운로드할 수 있도록 하려면 어떻게 해야 할까요?
+서버의 특정 경로를 보호하고 인증된 사용자만 접근할 수 있도록 해야 합니다.
+이를 위해 대다수의 플라스크 개발자는 Flask_Login 패키지를 사용합니다.
+
+고난이도 과제:
+Flask_Login 문서를 사용하여 /login 경로를 만들어보세요. /secrets 경로는 로그인한 사용자만 접근할 수 있도록 보호되어야 합니다.
+
+다음과 같이 만들어야 합니다.
+힌트 1: Flask_Login을 사용하도록 플라스크 앱을 구성해야 합니다. 
+https://flask-login.readthedocs.io/en/latest/#configuring-your-application
+힌트 2: user_loader 함수를 생성해야 합니다.
+https://flask-login.readthedocs.io/en/latest/#how-it-works
+힌트 3: 사용자 클래스에서 UserMixin을 구현해야 합니다.
+https://flask-login.readthedocs.io/en/latest/#your-user-class
+참고: Mixin은 파이썬에 다중 상속을 제공하는 방법으로 Mixin을 추가하는 방법은 다음과 같습니다.
+class MyClass(MixinClassB, MixinClassA, BaseClass):
+Mixin에 대한 자세한 정보 https://www.thedigitalcatonline.com/blog/2020/03/27/mixin-classes-in-python/
+힌트4: check_password_hash 함수를 사용하여 사용자의 비밀번호를 확인할 수 있습니다.
+https://werkzeug.palletsprojects.com/en/stable/en/1.0.x/utils/#werkzeug.security.check_password_hash
+힌트 5: 로그인 양식에 입력한 이메일로 사용자를 찾아야 합니다.
+힌트 6: 사용자가 성공적으로 로그인 또는 등록한 경우, login_user() 함수를 사용하여 인증해야 합니다.
+힌트 7: 인증된 사용자만 액세스할 수 있도록 /secrets과 /download 경로를 모두 보호해야 합니다.
+https://flask-login.readthedocs.io/en/latest/#flask_login.login_required
+
+해답 - https://gist.github.com/angelabauer/f53574e00338a08e989c3c983506a8ba
+
+
+
+# 플라스크 플래시 메시지
+사용자가 수행한 작업에 대한 피드백을 제공하고자 하는 경우가 생길 수 있습니다. 예를 들어 '로그인에 문제가 있었나요?', '잘못된 비밀번호를 입력했거나 이메일이 존재하지 않습니까?'와 같은 피드백을 전달하고 싶은 상황에서 로그인 페이지로 계속 리디렉션하는 대신 무엇이 잘못되었는지 알려준다면 좋은 사용자 경험을 제공할 수 있을 것입니다.
+이를 위한 가장 쉬운 방법은 플라스크 플래시(Flask Flash) 메시지를 사용하는 것입니다. 이는 템플릿으로 전송되어 한 번만 표시되는 메시지로, 페이지를 새로고침하면 사라집니다.
+https://flask.palletsprojects.com/en/1.1.x/patterns/flashing/
+
+1. 아래 예시와 같이 사용자의 이메일이 데이터베이스에 없는 경우, 사용자에게 플래시 메시지를 보내 이를 알리고 로그인 경로로 다시 리디렉션하도록 로그인 경로를 업데이트하세요.
+힌트: 로그인 페이지의 <p> 태그는 빨간색 텍스트로 표시됩니다.
+2. 아래 예시와 같이 check_password 함수가 False를 반환하면 사용자를 다시 로그인 페이지로 리디렉션할 때 플래시 메시지를 보내도록 로그인 경로를 업데이트하세요.
+3. 아래 예시와 같이 사용자가 입력한 이메일이 데이터베이스에 이미 존재할 경우 로그인 페이지로 리디렉션하고 이미 등록된 사용자임을 알리는 플래시 메시지가 표시되도록 /register 경로를 업데이트하세요.
+
+해답 - https://gist.github.com/angelabauer/b58cf1f98a9460eaf9a9adfd8d8b28e3
+
+
+# 인증상태를 템플릿에 전달하기
+사용자가 로그인할 때 홈페이지에 로그인/등록 버튼이 보이면 안 됩니다. 내비게이션 바에도 등록 또는 로그인이 보이면 안 됩니다.
+이렇게 되도록 base.html과 index.html에서 코드를 바꿀 수 있는지 시도해 보세요.
+이전 수업에서 base.html이 모든 페이지가 만들어지는 레이아웃 템플릿이라고 배웠던 것을 기억하세요.
+https://flask.palletsprojects.com/en/1.1.x/patterns/templateinheritance/
+
+해답 - https://gist.github.com/TheMuellenator/8f99d3f77114064f5aee8e8be0669fbd
--- python_advanced/day-68-start/main.py
+++ python_advanced/day-68-start/main.py
@@ -9,67 +9,154 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
 # CREATE DATABASE
-class Base(DeclarativeBase):
-    pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-db = SQLAlchemy(model_class=Base)
-db.init_app(app)
+# class Base(DeclarativeBase):
+#     pass
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+# db = SQLAlchemy(model_class=Base)
+# db.init_app(app)
 
-# CREATE TABLE IN DB
-class User(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(String(100), unique=True)
-    password: Mapped[str] = mapped_column(String(100))
-    name: Mapped[str] = mapped_column(String(1000))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+##CREATE TABLE
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+# db.create_all()
+
+
+# # CREATE TABLE IN DB
+# class User(db.Model):
+#     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+#     email: Mapped[str] = mapped_column(String(100), unique=True)
+#     password: Mapped[str] = mapped_column(String(100))
+#     name: Mapped[str] = mapped_column(String(1000))
  
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    # return render_template("index.html")
+    # Passing True or False if the user is authenticated. 
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        
+        # if User.query.filter_by(email=request.form.get('email')).first():
+        #     #User already exists
+        #     flash("You've already signed up with that email, log in instead!")
+        #     return redirect(url_for('login'))
+        
+        email = request.form.get('email')
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+        if user:
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+        
+        hash_and_salted_password = generate_password_hash(
+            request.form.get('password'),
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
 
         new_user = User(
             email=request.form.get('email'),
             name=request.form.get('name'),
-            password=request.form.get('password')
+            password=hash_and_salted_password,
         )
         
         db.session.add(new_user)
         db.session.commit()
+
+        #Log in and authenticate user after adding details to database.
+        login_user(new_user)
         
         # return redirect(url_for("secrets"))
         return redirect(url_for("secrets", name=new_user.name))
 
-    return render_template("register.html")
+    # return render_template("register.html")
+    # Passing True or False if the user is authenticated. 
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
-@app.route('/login')
+# @app.route('/login', methods=["GET", "POST"])
+# def login():
+#     if request.method == "POST":
+#         email = request.form.get('email')
+#         password = request.form.get('password')
+        
+#         #Find user by email entered.
+#         user = User.query.filter_by(email=email).first()
+        
+#         #Check stored password hash against entered password hashed.
+#         if check_password_hash(user.password, password):
+#             login_user(user)
+#             return redirect(url_for('secrets'))
+          
+#     return render_template("login.html")
+
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        result = db.session.execute(db.select(User).where(User.email == email))
+    
+        # user = User.query.filter_by(email=email).first()
+        user = result.scalar()
+        #Email doesn't exist
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        #Password incorrect
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        #Email exists and password correct
+        else:
+            login_user(user)
+            return redirect(url_for('secrets'))
 
+    # return render_template("login.html")
+    # Passing True or False if the user is authenticated. 
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
-@app.route('/secrets/<name>')
-def secrets(name):
-    return render_template("secrets.html", name=name)
+# @app.route('/secrets/<name>')
+# def secrets(name):
+#     return render_template("secrets.html", name=name)
 
-# @app.route('/secrets')
-# def secrets():
-#     return render_template("secrets.html")
+@app.route('/secrets')
+@login_required
+def secrets():
+    print(current_user.name)
+    # return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
+@login_required
 def download():
     return send_from_directory(directory='static', path="files/cheat_sheet.pdf")
 
